7b818553eb7ff6eb10a3be4dcb697351db7f510b
nitheshashwini <ashwini.rpem40@gmail.com>
2025-03-11 14:55:17 +0530

    Organization created

---
+++ src/test/java/organiztionmodule/createOrganization.java
@@ -0,0 +1,15 @@
+package organiztionmodule;
+
+import org.testng.annotations.Test;
+
+public class createOrganization 
+{
+	@Test
+	public void  createOrg()
+	{
+		System.out.println("execute createOrganization");
+		
+	}
+
+
+}
